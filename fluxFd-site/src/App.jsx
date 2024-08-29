94d02adcaffdaae5d9a494564c9d978db8571f43
Kael <jalcantarabedtc@gmail.com>
2024-08-29 11:07:18 +0800

    Init

--- fluxFd-site/src/App.jsx
+++ fluxFd-site/src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-
+import Home from './Pages/Home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="font-serif text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => setCount((count) => count + 1)}>
-      count is: {count}
-    </button>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
