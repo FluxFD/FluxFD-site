a94fd832fda81b862279c852e7a8841e6598f55d
Kael <jalcantarabedtc@gmail.com>
2024-08-29 11:18:34 +0800

    init

--- fluxFd-site/src/App.jsx
+++ fluxFd-site/src/App.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
+import NavBar from './Components/NavBar'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/nav" element={<NavBar />} />
         </Routes>
       </Router>
     </>
