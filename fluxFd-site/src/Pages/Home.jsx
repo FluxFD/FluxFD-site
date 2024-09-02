b66eb691b74320fca30502619fab1269b1b0e3c7
James Lumawag <64976921+Satsuu@users.noreply.github.com>
2024-09-02 20:38:04 +0800

    update

--- fluxFd-site/src/Pages/Home.jsx
+++ fluxFd-site/src/Pages/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>
-      <h1 className='font-light'> Home</h1>
+    <div className="h-full w-full min-h-screen flex flex-col justify-center">
+      <div className="md:w-1/2 font-mono font-bold text-4xl">
+        <p>
+          <span className="text-red-500">Transforming Ideas</span> into Reality
+        </p>
+        <p>
+          with <span className="text-red-500">Web and Mobile Expertise.</span>
+        </p>
+        <div className="mt-5 md:w-2/3">
+          <p class="font-light text-base text-wrap">
+            Dedicated to turning your unique ideas into reality by providing
+            innovative web and mobile solutions that enhance user experience and
+            drive business growth
+          </p>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-3 border-2 border-rose-500 rounded-lg divide-x">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
